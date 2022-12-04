b569869ec758a0e5dce4a76c43a373290fc34905
Eng-DO4 <mustafayasser1212@gmail.com>
2022-12-04 09:26:07 +0200

    hurray

--- udagram/udagram-frontend/src/environments/environment.ts
+++ udagram/udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://udagram-app-dev.us-east-1.elasticbeanstalk.com/api/v0'
+  apiHost: 'http://aws-dr4-bucket.s3-website-us-east-1.amazonaws.com/'
 };
 
 /*
